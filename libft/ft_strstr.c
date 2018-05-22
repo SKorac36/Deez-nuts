3405b221bd7ea2ec7ac3dbfe48d373e932702a1b
Stefan KORAC <skorac@e5r10p2.wethinkcode.co.za>
2018-05-22 09:10:29 +0200

    help me

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -6,23 +6,33 @@
 /*   By: skorac <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 11:49:49 by skorac            #+#    #+#             */
-/*   Updated: 2018/05/21 14:32:51 by skorac           ###   ########.fr       */
+/*   Updated: 2018/05/22 09:10:02 by skorac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <unistd.h>
+void	ft_putstr(char *str)
+{
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		write(1, &str[i] , 1);
+		i++;
+	}
+}
+§
 char	*ft_strstr(char *lrg, char *sml)
 {
 	int		i;
 	int		j;
 
 	i = 0;
-	j = 0;
-	if (sml[j] == '\0')
+	if (sml[0] == '\0')
 		return (lrg);
 	while (lrg[i] != '\0')
 	{
+		j = 0;
 		if (lrg[i] == sml[j] && sml[j] != '\0')
 		{
 			while (lrg[i] == sml[j])
@@ -39,4 +49,16 @@ char	*ft_strstr(char *lrg, char *sml)
 		i++;
 	}
 	return (NULL);
+}
+
+int		main()
+{
+	char *s1 = "my name is jeff";
+	char *s2 = "ne";
+	char *ret;
+	
+	ret = ft_strstr(s1, s2);
+	ft_putstr(ret);
+	write(1, "\n",1);
+	return (0);
 }
